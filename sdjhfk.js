ee64d7898daca2e474e63e34680422da629080a5
lootfi449 <138622697+lootfi449@users.noreply.github.com>
2024-01-17 19:22:43 +0300

    Update sdjhfk.js

--- sdjhfk.js
+++ sdjhfk.js
@@ -1,4 +1,4 @@
-const myElement = React.createElement('h4', {}, 'I do Je!');
+const myElement = React.createElement('h4', {} do Je!');
 
 const root = ReactOM.createRoot(document.getElementById('root'));
 root.render(myElement);
