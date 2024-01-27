c4f36aebb77d2deb8165421c000adad48b1cde4d
lootfi449 <138622697+lootfi449@users.noreply.github.com>
2024-01-27 18:28:45 +0300

    Update sdjhfk.js

--- sdjhfk.js
+++ sdjhfk.js
@@ -1,6 +1,6 @@
 const myElement = React.createElement('h9', {} do Je!');
 
-const root = ReactOM.createRot(document.getElementById('ropp'));
+const root = ReactOM.createRot(document.getElemenById('ropp'));
 root.render(myElement);
 
 import React from 'react';
